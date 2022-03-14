82c8d97b3557ec69d5e494f693a247584e079694
ZHANGLAOJIU <zhanglaojiuweizhu@gmail.COM>
2022-03-14 22:56:05 +0800

    1

--- try.py
+++ try.py
@@ -1,23 +1,24 @@
 from collections import defaultdict
 from collections import Counter
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        a = defaultdict(int)
-        a0 = Counter(s1)
-        for i in range(len(s2)):
-            if i >= len(s1):
-                a[s2[i - len(s1)]] -= 1
-            a[s2[i]] += 1
-            if a == a0:
-                return True
-        return False
+    def maxVowels(self, s: str, k: int) -> int:
+        a1 = {'a': 999,'e':999,'i':999,'o':999,'u':999}
+        a2 = defaultdict(int)
+        res = 0
+        for i in range(len(s)):
+            if i >= k:
+                a2[s[i - k]] -= 1
+            a2[s[i]] += 1
+            res = max(res, sum((a1 - (a2 - a1)).values()))
+        return res
 
 nums = [9,4,1,7]
 
-s1= "ab"
+s1 = "abciiidef"
+
 s2 = "eidbaooo"
 
 
 s = Solution()
-print(s.checkInclusion( s1,s2))
+print(s.maxVowels( s1,3))
 
